34b1f9085dc2b5fc025a880e5b19561fda37ff9d
alextech303 <alextech303@gmail.com>
2023-10-31 15:47:34 +0300

    correct task4_lab1

--- task_4/Program.cs
+++ task_4/Program.cs
@@ -1,11 +1,16 @@
-﻿
-
+﻿using System;
+/*Метод принимает 2 параметра   bool и получает результат всех булевых
+ операций над результат всех булевых операций над ними ( &, |, ^).
+ Возвращение результата – на ваше усмотрение. */
+    
+   
 bool x;
 bool y;
+int count = 0;
 
 while (true)
 {
-    Console.WriteLine("Введите значение x :");
+    Console.WriteLine("Введите значение параметра x :");
 
 if (bool.TryParse(Console.ReadLine(), out x))
 {
@@ -16,7 +21,7 @@ else Console.WriteLine("Неверный ввод");
 
 while (true)
 {
-    Console.WriteLine("Введите значение y :");
+    Console.WriteLine("Введите значение параметра y :");
 
     if (bool.TryParse(Console.ReadLine(), out y))
     {
@@ -26,13 +31,30 @@ while (true)
 }
 
 
-m(x, y);
-void m(bool x,bool y)
+bool[] m(bool x,bool y)
 {
     bool a = x & y;
     bool b = x | y;
     bool c = x ^ y;
-    Console.WriteLine("x & y = "+a);
-    Console.WriteLine("x | y = "+b);
-    Console.WriteLine("x ^ y = "+c);
+    bool[] array = new bool[3];
+    array[0]=a;
+    array[1]=b;
+    array[2]=c;
+    return array;
 }
+
+m(x, y);
+
+foreach (bool result in m(x, y))
+{
+    if (count==0)  
+        Console.WriteLine("x & y = "+result);
+           
+    if (count==1)
+        Console.WriteLine("x | y = "+result);
+            
+    if (count==2)
+         Console.WriteLine("x ^ y = "+result);
+    count++;
+}
+
