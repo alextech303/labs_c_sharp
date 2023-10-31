3f96e9d7fc45d1f9ba71b27f6ef0136bc733e00c
alextech303 <alextech303@gmail.com>
2023-10-31 14:30:34 +0300

    add task_4

---
+++ task_4/Program.cs
@@ -0,0 +1,38 @@
+﻿
+
+bool x;
+bool y;
+
+while (true)
+{
+    Console.WriteLine("Введите значение x :");
+
+if (bool.TryParse(Console.ReadLine(), out x))
+{
+    break;
+}
+else Console.WriteLine("Неверный ввод");
+}
+
+while (true)
+{
+    Console.WriteLine("Введите значение y :");
+
+    if (bool.TryParse(Console.ReadLine(), out y))
+    {
+        break;
+    }
+    else Console.WriteLine("Неверный ввод");
+}
+
+
+m(x, y);
+void m(bool x,bool y)
+{
+    bool a = x & y;
+    bool b = x | y;
+    bool c = x ^ y;
+    Console.WriteLine("x & y = "+a);
+    Console.WriteLine("x | y = "+b);
+    Console.WriteLine("x ^ y = "+c);
+}
---
+++ task_4_framework/ClassName.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_4_framework
+{
+    public  class ClassName
+    {
+
+        public int Name (int x,int y)
+        {
+            return 0;
+        }
+        static void Main(string[] args)
+        {
+        }
+    }
+}
